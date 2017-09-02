8d160b42583c6f14ed0d11f7cf332339611c819e
Bruno Dantas <oliveiradantas96@gmail.com>
2017-09-02 18:10:57 -0300

    [RENDERS AND CLASS] Modification view.py and create RegidtrationViews

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
 
-# Create your views here.
+# Arquivo: /apps/users/views.py
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+from django.core.urlresolvers import reverse_lazy, reverse
+
+from .forms import CustomUserCreationForm
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def login_view(request, *args, **kwargs):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('users:home'))
+
+    kwargs['extra_context'] = {'next': reverse('users:home')}
+    kwargs['template_name'] = 'users/login.html'
+    return login(request, *args, **kwargs)
+
+
+def logout_view(request, *args, **kwargs):
+    kwargs['next_page'] = reverse('users:home')
+    return logout(request, *args, **kwargs)
+
+
+class RegistrationView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = "users/register.html"
