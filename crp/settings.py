64e9cbf573660f6eecae2c9e0d7b7cbd611ebf00
Diego <wdresende@gmail.com>
2017-09-02 18:01:07 -0300

    [STRUCTURE] Remake app structure

--- crp/settings.py
+++ crp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+AUTH_USER_MODEL = "users.MyUser"
