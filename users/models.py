d09919ab1d7c2b6554272fa09bb1f64df0a5cf99
marthaDs <marthadantassilva@hotmail.com>
2017-09-02 17:28:31 -0300

    [CLASS] Create class of users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.translation import ugettext as _
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
+
+class EmailUserManager(BaseUserManager):
+    def create_user(self, *args, **kwargs):
+        email = kwargs["email"]
+        email = self.normalize_email(email)
+        password = kwargs["password"]
+        kwargs.pop("password")
+
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+
+        user = self.model(**kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, *args, **kwargs):
+        user = self.create_user(**kwargs)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
