c2b7968abd40c4d72b43ea47e69a71476860a88c
Diego <wdresende@gmail.com>
2017-09-08 20:48:30 -0300

    [UPDATE]Updating users models.py with attendants login

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,6 +5,28 @@ from django.utils.translation import ugettext as _
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 
+class EmailAttendantManager(BaseUserManager):
+    def create_attendant(self, *args, **kwargs):
+        email = kwargs["email"]
+        email = self.normalize_email(email)
+        password = kwargs["password"]
+        kwargs.pop("password")
+
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+
+        attendant = self.model(**kwargs)
+        attendant.set_password(password)
+        attendant.save(using=self._db)
+        return attendant
+
+    """
+    def create_superuser(self, *args, **kwargs):
+        user = self.create_user(**kwargs)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    """
 
 class EmailUserManager(BaseUserManager):
     def create_user(self, *args, **kwargs):
@@ -26,6 +48,27 @@ class EmailUserManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
         return user
+
+class MyAttendant(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name=_('Email address'),
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name=_('Nome'),
+        max_length=50,
+        blank=False,
+        help_text=_('Inform your name'),
+    )
+    last_name = models.CharField(
+        verbose_name=_('Sobrenome'),
+        max_length=50,
+        blank=False,
+        help_text=_('Inform your last name'),
+    )
+    USERNAME_FIELD = 'email'
+    objects = EmailAttendantManager()
+
 
 class MyUser(PermissionsMixin, AbstractBaseUser):
     email = models.EmailField(
