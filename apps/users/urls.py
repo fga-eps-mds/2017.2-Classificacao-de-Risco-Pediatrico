9428d2deac87d641f62847f0a12fcd26edc66182
Poulvilho <pv.paulovictor.vp@gmail.com>
2017-09-16 18:34:44 -0300

    [CODE] Create link teste

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
+    url(r'^teste/$', views.teste, name="teste"),
     url(r'^$', views.home, name="home"),
     url(r'^login/$', views.login_view, name="login"),
     url(r'^logout/$', views.logout_view, name="logout"),
