273f2d4e9c7bbe2a8a24836afbf71bbc433fa201
Bruno Dantas <oliveiradantas96@gmail.com>
2017-09-16 17:23:04 -0300

    [CODE] Create form RegistrationPacientForm

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,15 +1,19 @@
 # Arquivo: apps/users/forms.py
 from django.contrib.auth.forms import UserCreationForm
-from .models import Admin, Attendant
-
+from django import forms
+from .models import Admin, Attendant, Pacient
 
 class RegistrationAdminForm(UserCreationForm):
     class Meta:
         model = Admin
         fields = ['username', 'name', 'email', 'uf', 'city', 'neighborhood', 'street', 'block', 'number']
 
-
 class RegistrationAttendantForm(UserCreationForm):
     class Meta:
         model = Attendant
         fields = ['username', 'name', 'email', 'uf', 'city', 'neighborhood', 'street', 'block', 'number']
+
+class RegistrationPacientForm(forms.ModelForm):
+	class Meta:
+		model = Pacient
+		fields = ['name', 'cpf', 'guardian', 'birth_date', 'parents_name', 'uf', 'city', 'neighborhood', 'street', 'block', 'number' ]
