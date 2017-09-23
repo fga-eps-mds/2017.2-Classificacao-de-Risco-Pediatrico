b18b8cc18980776a6523a1a13ccba6f707a4f986
joaovitor3 <joao_vitor_04@hotmail.com>
2017-09-23 15:18:26 -0300

    [CODE]Update users forms

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,6 +1,6 @@
 # Arquivo: apps/users/forms.py
 from django.contrib.auth.forms import UserCreationForm
-from .models import Admin, Attendant, Address
+from .models import Admin, Attendant, Address, Staff, Recepcionist
 from django import forms
 
 
@@ -10,13 +10,19 @@ class AddressForm(forms.ModelForm):
         fields = ['uf', 'city', 'neighborhood', 'street', 'block', 'number']
 
 
-class RegistrationAdminForm(UserCreationForm):
+class RegistrationRecepcionistForm(UserCreationForm):
     class Meta:
-        model = Admin
-        fields = ['username', 'name', 'email', 'id_user']
+        model = Recepcionist
+        fields = ['email', 'id_user']
 
 
 class RegistrationAttendantForm(UserCreationForm):
     class Meta:
         model = Attendant
-        fields = ['username', 'name', 'email', 'id_user']
+        fields = ['name', 'email', 'id_user']
+
+
+class RegistrationAdminForm(UserCreationForm):
+    class Meta:
+        model = Admin
+        fields = ['name', 'email', 'id_user']
