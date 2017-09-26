2e9a288bce97e42c430db20e2a3576b36dbbfbdd
Victor Alves <victoragomide@gmail.com>
2017-09-26 14:48:36 -0300

    [DEBUG]Test in models.py

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,4 +1,6 @@
 # Arquivo: apps/users/models.py
+#teste
+
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 from django.utils.translation import ugettext as _
