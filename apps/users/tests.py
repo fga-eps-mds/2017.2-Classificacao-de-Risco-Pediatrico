bc484810cbafd3671e7014e444cff0aa901e8f19
Thiago Lima <thiagolima.unb@gmail.com>
2017-09-27 00:09:51 -0300

    [TEST] fixing flake8

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
+# from django.test import TestCase
 import pytest
-from apps.users.models import UserManager
+# from apps.users.models import UserManager
 # import apps.users.views
 # from apps.users.views import RegistrationAdminView
 # from factories import PatientFactory
@@ -14,7 +14,6 @@ class TestUsers:
         response = client.get('/user/')
         assert response.status_code == 200
 
-
     def test_login_view(self, client):
 
         # if this simple test is failing,
@@ -25,8 +24,10 @@ class TestUsers:
     def test_logout_view(self, client):
         # TODO: review this test:
         kwargs = {'email': 'email@gmail.com', 'password': '1234asdf'}
-        is_logged = client.login(username=kwargs["email"], password=kwargs["password"])
-        assert is_logged == False
+        is_logged = client.login(
+            username=kwargs["email"],
+            password=kwargs["password"])
+        assert is_logged is False
 
     def test_home_receptionist_view(self, client):
         response = client.get('/user/home/receptionist/')
