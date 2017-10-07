23982eda554edf277bea24039a14f0965d7d92b0
Bruno Dantas <oliveiradantas96@gmail.com>
2017-10-07 00:47:50 -0300

    [CODE] Create Staff tests.

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -36,8 +36,34 @@ class TestUsers:
         assert response.status_code == 200
 
     def test_user_get_full_name(self):
+        name = "Carlinhos Cabral"
+        user = Staff(name=name)
+        assert user.get_full_name() == name
+
+    def test_get_short_name(self):
         name = "Carlinhos"
         user = Staff(name=name)
+        assert user.get_short_name() == name
 
-        assert user.get_full_name() == name
+    def test_has_perm(self):
+        perm = None
+        user = Staff()
+        assert user.has_perm(perm) is True
+
+    def test_has_module_perms(self):
+        app_name = None
+        user = Staff()
+        assert user.has_module_perms(app_name) is True
+
+    def test_is_admin(self):
+        user_name = Staff(name='Bruno', is_admin=True)
+        assert user_name.is_admin is True
+
+    def test_is_not_admin(self):
+        user_name = Staff(name='Bruno')
+        assert user_name.is_admin is False
+
+    def test__str__(self):
+        user_email = Staff(email='bruno@gmail.com')
+        assert str(user_email) == 'bruno@gmail.com'
 
