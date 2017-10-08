b3e907b0c46ce02dc8798f4172df46e1e09aa3cf
joaovitor3 <joao_vitor_04@hotmail.com>
2017-10-07 23:05:09 -0300

    [CODE] Remove multimodelform

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
 from django.core.urlresolvers import reverse
-from multi_form_view import MultiModelFormView
+from django.contrib.auth.forms import UserCreationForm
+#from multi_form_view import MultiModelFormView
 
 from django.contrib.auth import authenticate
 
 from .forms import RegistrationStaffForm
-from .forms import AddressForm
 from .forms import RegistrationPatientForm
 
 from .models import Patient, Staff
@@ -59,6 +59,90 @@ def logout_view(request, *args, **kwargs):
     return logout(request, *args, **kwargs)
 
 
+def sign_up_profile(request):
+    if request.method == 'POST':
+        form = RegistrationStaffForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            staff = authenticate(username=username, password=raw_password)
+            login(request, 'users:login')
+            return redirect('users:login')
+    else:
+        form = RegistrationStaffForm()
+    return render(request, 'users/registerProfile.html', {'form': form})
+
+
+def sign_up_patient(request):
+    if request.method == 'POST':
+        form = RegistrationPatientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            patient = authenticate(username=username, password=raw_password)
+            login(request, 'users:home')
+            return redirect('users:home')
+    else:
+        form = RegistrationPatientForm()
+    return render(request, 'users/registerPatient.html', {'form': form})
+
+
+
+def register_patient(request):
+    """
+    Register a patient
+    """
+    return render(request, 'user/login', {})
+
+
+def show_pacient_view(request, cpf):
+    """
+    return rendered text from showPatient
+    """
+    patient = Patient.objects.filter(cpf=cpf)[0]
+    return render(request, 'users/showPatient.html', {'patient': patient})
+
+
+def home_receptionist_view(request):
+    """
+    return rendered text from homeReceptionist
+    """
+    return render(request, 'users/homeReceptionist.html')
+
+
+def admin_view(request):
+    """
+    return rendered text from homeReceptionist
+    """
+    return render(request, 'users/admin.html')
+
+
+def home_attendant_view(request):
+    """
+    return rendered text from homeAttendant
+    """
+    return render(request, 'users/homeAttendant.html')
+
+
+def manage_accounts_view(request):
+    staffs = Staff.objects.all()
+    return render(request, 'users/manageAccounts.html', { 'staffs':staffs })
+
+
+def edit_accounts_view(request, id_user):
+    staff = Staff.objects.filter(id_user=id_user)[0]
+    return render(request, 'users/editAccounts.html', { 'staffs':staffs })
+
+
+def staff_remove(request, id_user):
+    staff = Staff.objects.filter(id_user=id_user)
+    staff.delete()
+    return HttpResponseRedirect(reverse('users:manage_accounts'))
+
+
+'''
 class RegistrationStaffView(MultiModelFormView):
     form_classes = {
         'registration_staff_form': RegistrationStaffForm,
@@ -103,15 +187,9 @@ class RegistrationStaffView(MultiModelFormView):
         staff.address = forms['address_form'].save()
         staff.save()
         return super(RegistrationStaffView, self).forms_valid(forms)
+'''
 
-
-def register_patient(request):
-    """
-    Register a patient
-    """
-    return render(request, 'user/login', {})
-
-
+'''
 class RegistrationPatientView(MultiModelFormView):
     form_classes = {
         'registration_profile_form': RegistrationPatientForm,
@@ -156,50 +234,4 @@ class RegistrationPatientView(MultiModelFormView):
         patient.address = forms['address_form'].save()
         patient.save()
         return super(RegistrationPatientView, self).forms_valid(forms)
-
-
-def show_pacient_view(request, cpf):
-    """
-    return rendered text from showPatient
-    """
-    patient = Patient.objects.filter(cpf=cpf)[0]
-    return render(request, 'users/showPatient.html', {'patient': patient})
-
-
-def home_receptionist_view(request):
-    """
-    return rendered text from homeReceptionist
-    """
-    return render(request, 'users/homeReceptionist.html')
-
-
-def admin_view(request):
-    """
-    return rendered text from homeReceptionist
-    """
-    return render(request, 'users/admin.html')
-
-
-def home_attendant_view(request):
-    """
-    return rendered text from homeAttendant
-    """
-    return render(request, 'users/homeAttendant.html')
-
-
-def manage_accounts_view(request):
-    staffs = Staff.objects.all()
-    return render(request, 'users/manageAccounts.html', { 'staffs':staffs })
-
-def manage_accounts_search_view(request, id_user):
-    staffs = Staff.objects.filter(id_user=id_user)[0]
-    return render(request, 'users/manageAccounts.html', { 'staffs':staffs })
-
-def edit_accounts_view(request, id_user):
-    staff = Staff.objects.filter(id_user=id_user)[0]
-    return render(request, 'users/editAccounts.html', { 'staff':staff })
-
-def staff_remove(request, id_user):
-    staff = Staff.objects.filter(id_user=id_user)
-    staff.delete()
-    return HttpResponseRedirect(reverse('users:manage_accounts'))
+'''
