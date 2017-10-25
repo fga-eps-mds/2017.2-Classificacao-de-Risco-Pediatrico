cd0910adafee3f07ecffa97ece128b293ebf6a79
Diego <wdresende@gmail.com>
2017-10-25 00:25:26 -0200

    [CODE] Fix Flake8 Errors

--- apps/risk_rating/ml_classifier.py
+++ apps/risk_rating/ml_classifier.py
@@ -4,21 +4,22 @@ import numpy as np
 
 
 def classify_csv():
-    df = pd.read_csv('apps/risk_rating/class_menos_28.csv', true_values=["Sim"], false_values=["Não", "Nao"])
+    df = pd.read_csv('apps/risk_rating/class_menos_28.csv',
+                     true_values=["Sim"], false_values=["Não", "Nao"])
     df = df.drop(df.columns[[0, 2, 30]], axis=1)
 
     columnsTitles = list(df.columns.values)
     columnsTitles[27], columnsTitles[28] = columnsTitles[28], columnsTitles[27]
     df = df.reindex(columns=columnsTitles)
 
-
     # target_names = np.array(['Ambulatorial', 'Grave'], dtype='<U12')
     df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
 
-    train, test = df[df['is_train']==True], df[df['is_train']==False]
+    train = df[df['is_train'] == True]  # noqa: E712
+    test = df[df['is_train'] == False]  # noqa: E712
 
     print('Number of observations in the training data:', len(train))
-    print('Number of observations in the test data:',len(test))
+    print('Number of observations in the test data:', len(test))
 
     features = df.columns[1:12]
 
@@ -31,7 +32,8 @@ def classify_csv():
     preds = df['Classificacao'].unique()[clf.predict(test[features])]
 
     print('\n'*3)
-    print(pd.crosstab(test['Classificacao'], preds, rownames=['Actual'], colnames=['Predicted']))
+    print(pd.crosstab(test['Classificacao'], preds,
+                      rownames=['Actual'], colnames=['Predicted']))
     print('\n'*3)
 
     print(list(zip(train[features], clf.feature_importances_)))
