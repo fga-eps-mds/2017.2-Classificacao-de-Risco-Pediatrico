3e1c0edb2b6e0926fc25b5fdf65b87cfc723a447
joaovitor3 <joao_vitor_04@hotmail.com>
2017-10-25 19:31:48 -0200

    Signed-off-by: joaovitor3 <joao_vitor_04@hotmail.com> Co-authored by: marceloabk <marceloabk@hotmail.com>

--- crp/static/js/home.js
+++ crp/static/js/home.js
@@ -1,28 +1,7 @@
 $(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
 
-    trigger.click(function () {
-      hamburger_cross();
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
     });
 
-    function hamburger_cross() {
-
-      if (isClosed == true) {
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });
 });
