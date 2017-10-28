f45066f593438810747bafa8e79a643937d50d24
marthaDs <marthadantassilva@hotmail.com>
2017-10-28 14:52:31 -0200

    [CODE] updating modal.js
    Co-authored-by: Paulo Victor <pv.paulovictor.vp@gmail.com>

--- crp/static/js/modal.js
+++ crp/static/js/modal.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
   $('form').on('hidden.bs.modal', function (e) {
     console.log("oi");
-  //   $(this)
-  //     .find("input,textarea,select")
-  //        .val('')
-  //        .end()
-  //     .find("input[type=checkbox], input[type=radio]")
-  //        .prop("checked", false)
-  //        .end()
-  //     .find("input[type=checkbox], input[type=radio]")
-  //       .removeAttr('checked')
-  //       .end();
   $(this).trigger('reset');
   })
 } );
