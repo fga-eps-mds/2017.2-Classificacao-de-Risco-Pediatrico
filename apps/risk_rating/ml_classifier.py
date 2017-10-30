bbe53985c025747c37249135d75b9c7088f722d6
marthaDs <marthadantassilva@hotmail.com>
2017-10-30 21:43:53 -0200

    [CODE] Updating machine learning

--- apps/risk_rating/ml_classifier.py
+++ apps/risk_rating/ml_classifier.py
@@ -3,39 +3,48 @@ import pandas as pd
 import numpy as np
 
 
-def classify_csv():
-    df = pd.read_csv('apps/risk_rating/class_menos_28.csv',
-                     true_values=["Sim"], false_values=["Não", "Nao"])
-    df = df.drop(df.columns[[0, 2, 30]], axis=1)
+class MachineLearning:
 
-    columnsTitles = list(df.columns.values)
-    columnsTitles[27], columnsTitles[28] = columnsTitles[28], columnsTitles[27]
-    df = df.reindex(columns=columnsTitles)
+    def __init__(self):
+        self.__data_frame = self.read_csv()
+        self.__features = self.__data_frame.columns[:28]
+        self.__clf = self.random_forest(self.__data_frame, self.__features)
+        self.__target_names = self.get_target_names(self.__data_frame)
 
-    # target_names = np.array(['Ambulatorial', 'Grave'], dtype='<U12')
-    df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+    def classify_patient(self, patient):
+        predicted = self.__clf.predict(patient)
+        return self.__target_names[predicted[0]]
 
-    train = df[df['is_train'] == True]  # noqa: E712
-    test = df[df['is_train'] == False]  # noqa: E712
+    def calc_probabilities(self, patient):
+        probabilities = self.__clf.predict_proba(patient)
+        return probabilities
 
-    print('Number of observations in the training data:', len(train))
-    print('Number of observations in the test data:', len(test))
+    def random_forest(self, data_frame, features):
+        # factorize the classification possibilities
+        y = pd.factorize(data_frame['Classificacao'])[0]
+        clf = self.train_the_classifier(features, data_frame, y)
+        return clf
 
-    features = df.columns[1:12]
+    def train_the_classifier(self, features, data_frame, y):
+        clf = RandomForestClassifier(n_jobs=2)
+        clf.fit(data_frame[features], y)
+        return clf
 
-    y = pd.factorize(train['Classificacao'])[0]
+    def feature_importance(self):
+        return list(zip(self.__data_frame[self.__features], self.__clf.feature_importances_))
 
-    clf = RandomForestClassifier(n_jobs=2)
+    def read_csv(self):
+        df = pd.read_csv('apps/risk_rating/class_menos_28.csv',
+                         true_values=["Sim"], false_values=["Não", "Nao"])
+        df = df.drop(df.columns[[0, 2, 30]], axis=1)
+        columns_titles = list(df.columns.values)
 
-    clf.fit(train[features], y)
+        # changing last and penultimate values
+        columns_titles[27], columns_titles[28] = columns_titles[28], columns_titles[27]
+        df = df.reindex(columns=columns_titles)
 
-    preds = df['Classificacao'].unique()[clf.predict(test[features])]
+        return df
 
-    print('\n'*3)
-    print(pd.crosstab(test['Classificacao'], preds,
-                      rownames=['Actual'], colnames=['Predicted']))
-    print('\n'*3)
-
-    print(list(zip(train[features], clf.feature_importances_)))
-
-    print(clf.predict_proba(test[features])[0:10])
+    def get_target_names(self, data_frame):
+        duplicateless_df = data_frame['Classificacao'].drop_duplicates()
+        return duplicateless_df.values.tolist()
