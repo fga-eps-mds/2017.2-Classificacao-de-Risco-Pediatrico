871a02930b20e4a21b70c43a9f6c91746353c807
marthaDs <marthadantassilva@hotmail.com>
2017-10-30 23:54:08 -0200

    [CODE] view to do risk rating into classification modal
    Co-authored-by: wdresende <wdresende@gmail.com>

--- apps/users/views.py
+++ apps/users/views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
+from apps.risk_rating.ml_classifier import MachineLearning
 
 from apps.users.forms import RegistrationStaffForm
 from apps.users.forms import RegistrationPatientForm
@@ -14,6 +15,7 @@ from apps.users.forms import EditPatientForm
 
 from .models import Patient, Staff
 
+ml = MachineLearning()
 
 def login_view(request, *args, **kwargs):
     if request.method == "POST":
@@ -47,16 +49,93 @@ def home(request):
     patients = Patient.objects.all()
 
     if request.method == "POST":
-        patient_classification = request.POST.get("classification")
-        patient_id = request.POST.get("patient")
+        if request.POST.get("classification"):
+            patient_classification = request.POST.get("classification")
+            patient_id = request.POST.get("patient")
 
-        patient = Patient.objects.get(id=patient_id)
-        patient.classification = patient_classification
+            patient = Patient.objects.get(id=patient_id)
+            patient.classification = patient_classification
 
-        patient.save()
+            patient.save()
+        else:
+            dispineia = check_patient_problem(request.POST.get("dispineia"))
+            ictericia = check_patient_problem(request.POST.get("ictericia"))
+            consciencia = check_patient_problem(request.POST.get("consciência"))
+            cianose = check_patient_problem(request.POST.get("cianose"))
+            febre = check_patient_problem(request.POST.get("febre"))
+            solucos = check_patient_problem(request.POST.get("solucos"))
+            prostracao = check_patient_problem(request.POST.get("prostracao"))
+            vomitos = check_patient_problem(request.POST.get("vomitos"))
+            tosse = check_patient_problem(request.POST.get("tosse"))
+            coriza = check_patient_problem(request.POST.get("coriza"))
+            obstrucaoNasal = check_patient_problem(request.POST.get("obstrucaoNasal"))
+            convulsaoMomento = check_patient_problem(request.POST.get("convulsaoMomento"))
+            diarreia = check_patient_problem(request.POST.get("diarreia"))
+            choroIncosolavel = check_patient_problem(request.POST.get("choroIncosolavel"))
+            dificuldadeEvacuar = check_patient_problem(request.POST.get("dificuldadeEvacuar"))
+            naoSugaSeio = check_patient_problem(request.POST.get("naoSugaSeio"))
+            manchaPele = check_patient_problem(request.POST.get("manchaPele"))
+            salivacao = check_patient_problem(request.POST.get("salivacao"))
+            queda = check_patient_problem(request.POST.get("queda"))
+            chiadoPeito = check_patient_problem(request.POST.get("chiadoPeito"))
+            diminuicaoDiurese = check_patient_problem(request.POST.get("diminuicaoDiurese"))
+            dorAbdominal = check_patient_problem(request.POST.get("dorAbdominal"))
+            dorOuvido = check_patient_problem(request.POST.get("dorOuvido"))
+            fontanelaAbaulada = check_patient_problem(request.POST.get("fontanelaAbaulada"))
+            secrecaoUmbigo = check_patient_problem(request.POST.get("secrecaoUmbigo"))
+            secrecaoOcular = check_patient_problem(request.POST.get("secrecaoOcular"))
+            sangueFezes = check_patient_problem(request.POST.get("sangueFezes"))
+            convulsaoHoje = check_patient_problem(request.POST.get("convulsaoHoje"))
+
+            patient = [[
+                dispineia,
+                ictericia,
+                consciencia,
+                cianose,
+                febre,
+                solucos,
+                prostracao,
+                vomitos,
+                tosse,
+                coriza,
+                obstrucaoNasal,
+                convulsaoMomento,
+                diarreia,
+                choroIncosolavel,
+                dificuldadeEvacuar,
+                naoSugaSeio,
+                manchaPele,
+                salivacao,
+                queda,
+                chiadoPeito,
+                diminuicaoDiurese,
+                dorAbdominal,
+                dorOuvido,
+                fontanelaAbaulada,
+                secrecaoUmbigo,
+                secrecaoOcular,
+                sangueFezes,
+                convulsaoHoje,
+            ]]
+
+            probability = ml.calc_probabilities(patient)
+            classification = ml.classify_patient(patient)
+            impact_list = ml.feature_importance()
+
+            print(probability)
+            print(classification)
+            print(impact_list)
 
     return render(request, 'users/user_home/main_home.html',
                            {'patients': patients})
+
+def check_patient_problem(problem):
+    if problem is not None:
+        problem = 1
+    else:
+        problem = 0
+
+    return problem
 
 
 def logout_view(request, *args, **kwargs):
