d5e8c412900848353cdae7a2b0a4b2f60c4da2b6
Bruno Dantas <oliveiradantas96@gmail.com>
2017-11-03 20:34:23 -0200

    [CODE] Debuging animation from LandingPage template.

--- crp/static/js/landingPage/waypoint.js
+++ crp/static/js/landingPage/waypoint.js
@@ -1,9 +1,5 @@
   var $animate = $('.animate');
 
-  $animate.waypoint(function (direction) {
-    if (direction == 'down') {
+  $animate.waypoint(function () {
         $animate.addClass('js-animated');
-    }else {
-        $animate.removeClass('js-animated');
-    }
   }, {offset:'90%'});
