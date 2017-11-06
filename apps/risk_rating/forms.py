36ce4243a8250331bbd245b41e26bc63a57cd347
wdresende <wdresende@gmail.com>
2017-11-06 21:51:33 -0200

    [CODE] Creating form to get symptoms for patients under 28 days

---
+++ apps/risk_rating/forms.py
@@ -0,0 +1,16 @@
+# Arquivo: apps/risk_rating/forms.py
+from django import forms
+from apps.users.models import Patient
+from apps.risk_rating.models import ClinicalState_28d
+
+class ClinicalState_28dForm(forms.ModelForm):
+    """
+    Defining filds for under 28 days patient's clinical state
+    """
+    class Meta:
+        model = ClinicalState_28d
+        fields = ['patient', 'dispineia', 'ictericia', 'perdada_consciencia', 'cianose', 'febre',
+                  'solucos', 'prostacao', 'vomitos', 'coriza', 'obstrucao_nasal', 'convulcao_no_momento',
+                  'diarreia', 'choro_inconsolavel', 'dificuldade_evacuar', 'nao_suga_seio', 'manchas_na_pele',
+                  'salivacao', 'chiado_no_peito', 'diminuicao_da_diurese', 'dor_abdominal', 'fontanela_abaulada',
+                  'secrecao_no_umbigo', 'secrecao_ocular', 'convulcao_hoje']
