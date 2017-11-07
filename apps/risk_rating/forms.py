e5cd02f9313beedb3eda684858c6bf271e51e9c2
Diego <wdresende@gmail.com>
2017-11-07 20:27:48 -0200

    [CODE] Creating clinical state (2m-3y) and it's form

--- apps/risk_rating/forms.py
+++ apps/risk_rating/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from apps.users.models import Patient
 from apps.risk_rating.models import ClinicalState_28d
 from apps.risk_rating.models import ClinicalState_29d_2m
+from apps.risk_rating.models import ClinicalState_2m_3y
 
 class ClinicalState_28dForm(forms.ModelForm):
     """
@@ -27,3 +28,15 @@ class ClinicalState_29d_2mForm(forms.ModelForm):
                   'diarreia', 'dificuldade_evacuar', 'nao_suga_seio', 'manchas_na_pele',
                   'salivacao', 'chiado_no_peito', 'diminuicao_da_diurese', 'dor_abdominal', 'fontanela_abaulada',
                   'secrecao_no_umbigo', 'secrecao_ocular', 'sangue_nas_fezes','convulsao_hoje']
+
+class ClinicalState_2m_3yForm(forms.ModelForm):
+    """
+    Defining filds patients (29 days and 2 months old) clinical state
+    """
+    class Meta:
+        model = ClinicalState_2m_3y
+        fields = ['patient_id2', 'dispineia', 'ictericia', 'perdada_consciencia', 'cianose', 'febre',
+                  'solucos', 'prostracao', 'vomitos', 'tosse', 'coriza', 'obstrucao_nasal', 'convulcao_no_momento',
+                  'diarreia', 'dificuldade_evacuar', 'nao_suga_seio', 'manchas_na_pele',
+                  'salivacao', 'chiado_no_peito', 'diminuicao_da_diurese', 'dor_abdominal', 'fontanela_abaulada',
+                  'secrecao_no_umbigo', 'secrecao_ocular']
