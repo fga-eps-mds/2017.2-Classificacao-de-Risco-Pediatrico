1d42d61851385a6c586c9ac2a1cc3c2949d1bcab
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-11-07 19:45:30 -0200

    [CODE] Creating ClinicalState_29d_2m Class and it's Form

--- apps/risk_rating/forms.py
+++ apps/risk_rating/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from apps.users.models import Patient
 from apps.risk_rating.models import ClinicalState_28d
+from apps.risk_rating.models import ClinicalState_29d_2m
 
 class ClinicalState_28dForm(forms.ModelForm):
     """
@@ -9,8 +10,20 @@ class ClinicalState_28dForm(forms.ModelForm):
     """
     class Meta:
         model = ClinicalState_28d
-        fields = ['patient_id', 'dispineia', 'ictericia', 'perdada_consciencia', 'cianose', 'febre',
+        fields = ['patient_id1', 'dispineia', 'ictericia', 'perdada_consciencia', 'cianose', 'febre',
                   'solucos', 'prostracao', 'vomitos', 'tosse', 'coriza', 'obstrucao_nasal', 'convulcao_no_momento',
                   'diarreia', 'choro_inconsolavel', 'dificuldade_evacuar', 'nao_suga_seio', 'manchas_na_pele',
+                  'salivacao', 'chiado_no_peito', 'diminuicao_da_diurese', 'dor_abdominal', 'fontanela_abaulada',
+                  'secrecao_no_umbigo', 'secrecao_ocular', 'sangue_nas_fezes','convulsao_hoje']
+
+class ClinicalState_29d_2mForm(forms.ModelForm):
+    """
+    Defining filds patients (29 days and 2 months old) clinical state
+    """
+    class Meta:
+        model = ClinicalState_29d_2m
+        fields = ['patient_id2', 'dispineia', 'ictericia', 'perdada_consciencia', 'cianose', 'febre',
+                  'solucos', 'prostracao', 'vomitos', 'tosse', 'coriza', 'obstrucao_nasal', 'convulcao_no_momento',
+                  'diarreia', 'dificuldade_evacuar', 'nao_suga_seio', 'manchas_na_pele',
                   'salivacao', 'chiado_no_peito', 'diminuicao_da_diurese', 'dor_abdominal', 'fontanela_abaulada',
                   'secrecao_no_umbigo', 'secrecao_ocular', 'sangue_nas_fezes','convulsao_hoje']
