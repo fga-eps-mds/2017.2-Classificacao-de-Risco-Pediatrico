c34569aa29808fcfc4d639f9daafea6c21f7a71f
Marcelo Cristiano <marceloabk@hotmail.com>
2017-11-26 17:42:55 -0200

    [CODE] Correcting tabl ordering

--- crp/static/js/home.js
+++ crp/static/js/home.js
@@ -1,32 +1,29 @@
-$(document).ready(function() {
-      var table = $('#example').DataTable( {
-        order: [[2,'desc']],
-        responsive: true,
-        "lengthMenu": [ 50, 75, 100 ],
-        "columnDefs": [
-            {
-                "orderable": false,
-                "targets": [ 0,1,2,3,4 ]
-            }
-        ],
-        "language": {
-            "lengthMenu": "Mostrar _MENU_ por página",
-            "zeroRecords": "Nenhum paciente encontrado",
-            "info": "Mostrando página _PAGE_ de _PAGES_",
-            "infoEmpty": "Nenhuma informação disponível",
-            "infoFiltered": "(Filtrado de _MAX_ registros)",
-            "search": "Procurar:",
-            "paginate": {
-                "previous": "Anterior",
-                "next": "Próximo"
-            }
-        }
-    } );
+$(document).ready(function () {
+  $('#example').DataTable({
+    "responsive": true,
+    "order": [[2, 'desc']],
+    "columnDefs": [{
+      "targets": "_all",
+      "orderable": false
+    }],
+    "lengthMenu": [50, 75, 100],
+    "language": {
+      "lengthMenu": "Mostrar _MENU_ por página",
+      "zeroRecords": "Nenhum paciente encontrado",
+      "info": "Mostrando página _PAGE_ de _PAGES_",
+      "infoEmpty": "Nenhuma informação disponível",
+      "infoFiltered": "(Filtrado de _MAX_ registros)",
+      "search": "Procurar:",
+      "paginate": {
+        "previous": "Anterior",
+        "next": "Próximo"
+      }
+    }
+  });
 
   $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
   });
-
 
   $('.classification').on('click', function () {
 
@@ -34,13 +31,13 @@ $(document).ready(function() {
     var $inputs = $modal.find(':input');
 
     var values = {};
-    $inputs.each(function() {
+    $inputs.each(function () {
       if (this.name === "patient_id" ||
-          this.name === "form1" ||
-          this.name === "form2" ||
-          this.name === "form3" ||
-          this.name === "form4" ||
-          this.name === "form5" ) {
+        this.name === "form1" ||
+        this.name === "form2" ||
+        this.name === "form3" ||
+        this.name === "form4" ||
+        this.name === "form5") {
         values[this.name] = $(this).val();
       } else {
         values[this.name] = $(this).prop('checked');
@@ -59,7 +56,7 @@ $(document).ready(function() {
           $('#classify-patient-' + data["patient_id"]).text("Recomendado encaminhar para "
             + data["classification"]);
           $('#probability-' + data["patient_id"]).text("Porcentagem de Confiabilidade: "
-            + (Math.max.apply(Math, data["probability"][0])*100).toFixed(1) + "%");
+            + (Math.max.apply(Math, data["probability"][0]) * 100).toFixed(1) + "%");
         }
       }
     });
