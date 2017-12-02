86f03d6cf87c677ec97b0786310e8e4623e38190
Diego <wdresende@gmail.com>
2017-12-02 20:03:00 -0200

    [CODE] Created new graphics for each age range

--- apps/risk_rating/models.py
+++ apps/risk_rating/models.py
@@ -22,8 +22,7 @@ class Symptoms_28d(models.Model):
 
     ictericia = models.BooleanField(
         verbose_name=("Icterícia"),
-        default=False,
-        blank=True
+        default=False, blank=True
     )
 
     perdada_consciencia = models.BooleanField(
@@ -214,6 +213,13 @@ class Symptoms_29d_2m(models.Model):
     class Meta:
         abstract = True
 
+    date = models.DateField(
+        verbose_name='Data',
+        blank=False,
+        auto_now=True
+    )
+
+    # symptoms:
     dispineia = models.BooleanField(
         verbose_name=("Dispinéia"),
         default=False,
@@ -408,6 +414,13 @@ class Symptoms_2m_3y(models.Model):
     class Meta:
         abstract = True
 
+    date = models.DateField(
+        verbose_name='Data',
+        blank=False,
+        auto_now=True
+    )
+
+    # symptoms:
     dispineia = models.BooleanField(
         verbose_name=("Dispinéia"),
         default=False,
@@ -590,6 +603,13 @@ class Symptoms_3y_10y(models.Model):
     class Meta:
         abstract = True
 
+    date = models.DateField(
+        verbose_name='Data',
+        blank=False,
+        auto_now=True
+    )
+
+    # symptoms:
     perdada_consciencia = models.BooleanField(
         verbose_name=("Perda de Consciência"),
         default=False,
@@ -909,6 +929,13 @@ class Symptoms_10yMore(models.Model):
     class Meta:
         abstract = True
 
+    date = models.DateField(
+        verbose_name='Data',
+        blank=False,
+        auto_now=True
+    )
+
+    # symptoms:
     mais_de_72h_febre = models.BooleanField(
         verbose_name=("Febre a mais de 72 horas"),
         default=False,
