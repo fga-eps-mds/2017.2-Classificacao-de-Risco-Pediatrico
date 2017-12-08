7507ffce5be16b8b966c869acf1841cc7ca1617f
Anderson Silva <andersoonn.ams@gmail.com>
2017-12-08 16:11:13 -0200

    [CODE] Refactoring structure of steps

--- features/classification.feature
+++ features/classification.feature
@@ -1,13 +1,11 @@
 Feature: In order to make classifications
 
   Scenario Outline:
-    Given I register an employee: <name>, <id_number> and <profile_number>
-    Given I register patient: <name_patient>, <id_number_patient> and <profile_number_patient>and: <email> and <password>
+    Given I register an employee: <name_user>, <id_number_user> and <profile_number_user>
+    Given I register a patient: <name_patient>, <id_number_patient> and <profile_number_patient>
     And I realize a classification: <id_number_patient>
     Then The classification must be updated
 
     Examples:
-      | name            | id_number | profile_number | url            | name_patient    | id_number_patient | profile_number_patient | email                       | password         | url   |
-      | selenium-user-1 | 1         | 1              | login          | selenium-user-3 | 1                 | 2                      | selenium-user-3-3@gmail.com | selenium-user123 | home/ |
-
-
+      | name_user            | id_number_user | profile_number_user | url_user            | name_patient    | id_number_patient | profile_number_patient | email_                       | password_         | url_   |
+      | selenium-user-1      | 1              | 1                   | login               | selenium-user-3 | 1                 | 2                      | selenium-user-3-3@gmail.com  | selenium-user123  | home/  |
--- features/steps/classification.py
+++ features/steps/classification.py
@@ -2,26 +2,26 @@ from behave import step
 from selenium.webdriver.support.ui import Select
 
 
-@step(' I register an employee: {name_employee},' +
-      ' {id_number} and {profile_number}')
-def register_user(context, name_employee, id_number, profile_number):
+@step(u'I register an employee: {name_user},'
+' {id_number_user} and {profile_number_user}')
+def register_user(context, name_user, id_number_user, profile_number_user):
     context.browser.find_element_by_name(
-        "name").send_keys(name_employee)
+        "name").send_keys(name_user)
 
     Select(context.browser.find_element_by_id(
-        'dropdown-profile')).select_by_value(profile_number)
+        'dropdown-profile')).select_by_value(profile_number_user)
 
     context.browser.find_element_by_name(
-        "email").send_keys(f"{name_employee}-{id_number}@gmail.com")
+        "email").send_keys(f"{name_user}-{id_number_user}@gmail.com")
 
     context.browser.find_element_by_name(
-        "password1").send_keys("selenium-user123")
+        "password1").send_keys("selenium-user1234")
 
     context.browser.find_element_by_name(
-        "password2").send_keys("selenium-user123")
+        "password2").send_keys("selenium-user1234")
 
     context.browser.find_element_by_name(
-        "id_user").send_keys(id_number)
+        "id_user").send_keys(id_number_user)
 
     # dropdown state
     Select(context.browser.find_element_by_id(
@@ -32,18 +32,20 @@ def register_user(context, name_employee, id_number, profile_number):
     context.browser.find_element_by_name("neighborhood").send_keys("UnB")
     context.browser.find_element_by_name("block").send_keys("----")
     context.browser.find_element_by_name("number").send_keys("10")
-
     context.browser.find_element_by_id('button-cadastrar').click()
 
-@step('I register patient')
-def register_patient(context):
+
+@step('I register a patient: {name_patient}, {id_number_patient} and {profile_number_patient}')
+def register_patient(context, name_patient, id_number_patient, profile_number_patient):
+    print("teste")
+    print(browser.current_url)
     context.browser.find_element_by_id('register-patient').click()
     Select(context.browser.find_element_by_id(
         'id_age_range')).select_by_value('0')
     context.browser.find_element_by_id('register-patient-button').click()
 
 
-@step('I register patient: {id_number}')
+@step('I realize a classification: {id_number}')
 def classification(context):
     context.browser.find_element_by_id('name_patient').click()
     context.browser.find_element_by_name('dispineia').click()
