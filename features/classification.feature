7fd0946eb5714770e20789b27139a82f0fcdd9fa
Anderson Silva <andersoonn.ams@gmail.com>
2017-12-10 23:43:35 -0200

    [CODE] Trying to acess modal of classification

--- features/classification.feature
+++ features/classification.feature
@@ -1,15 +1,18 @@
 Feature: In order to make classifications
 
-  Scenario Outline:
+  Scenario Outline: Do classification
+    #create a patient
     When I access register/user/
-    Given I register an employee: <name>, <id_number> and <profile_number_user>
+    And  fill information fields with data: <name>, <id_number> and <profile_number>
+    And I click the submit button
     And I log into server with data: <email> and <password>
+    And I click in register patient
+    And I fill patient form
     Then it should redirect me to the <url> page
-    Given I register a patient: <name_patient>, <id_number_patient> and <profile_number_patient>
-    Then it should redirect me to the <url> page
-    And I realize a classification: <id_number_patient>
-    Then The classification must be updated
+    When I click in patient: <id_patient>
+    And In modal a chose the symptons
+    Then Should update de classifications
 
     Examples:
-      | name                 | id_number      | profile_number_user | url_user            | name_patient    | id_number_patient | profile_number_patient | email                       | password         | url   |
-      | selenium-user-3      | 3              | 2                   | login               | selenium-user-3 | 3                 | 2                      | selenium-user-3-3@gmail.com | selenium-user123 | home/ |
+      | name            | id_number | profile_number | email                       | password         | url   | id_patient |
+      | selenium-user-3 | 3         | 2              | selenium-user-3-3@gmail.com | selenium-user123 | home/ | 1          |
--- features/steps/classification.py
+++ features/steps/classification.py
@@ -1,62 +1,63 @@
 from behave import step
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
-@step('I register an employee: {name_employee},' +
-      ' {id_number} and {profile_number_user}')
-def register_user(context, name_employee, id_number, profile_number_user):
-    context.browser.find_element_by_name(
-        "name").send_keys(name_employee)
 
-    Select(context.browser.find_element_by_id(
-        'dropdown-profile')).select_by_value(profile_number_user)
+# @step('I realize a classification: {id_number_patient}')
+# def classification(context, id_number_patient):
+#
+#     # print(context.browser.current_url)
+#     context.browser.find_element_by_id('id_modal-{id_number_patient}').click()
+#
+#     wait = WebDriverWait(context.browser, 10)
+#     element = wait.until(EC.element_to_be_clickable((By.NAME, "dispineia")))
+#
+#
+#     context.browser.find_element_by_name("dispineia").click()
+#     context.browser.find_element_by_name("prostracao").click()
+#
+#     element = wait.until(EC.element_to_be_clickable((By.ID, "modal_id")))
+#     context.browser.find_element_by_id('modal_id').click()#open second modal
+#
+#     element = wait.until(EC.element_to_be_clickable((By.ID, "classification_save")))
+#     context.browser.find_element_by_id('classification_save').click()#confirm classification
+#
+#
+#
+# @step('The classification must be updated')
+# def verification(context):
+#     print (context.browser.current_url)
+#     title = context.browser.title
+#     assert 'CRP' in title
 
-    context.browser.find_element_by_name(
-        "email").send_keys(f"{name_employee}-{id_number}@gmail.com")
+@when(u'I click in patient: {id_patient}')
+def acess_patient(context, id_patient):
+    context.browser.find_element_by_id(f'id_modal-{id_patient}').click()
+    # context.browser.find_element_by_css_selector(f'#risk_ranting{id_patient} input[name="dispineia"]').click()
+    #wait 1
+    # wait = WebDriverWait(context.browser, 10)
+    # element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'#risk_ranting{id_patient} input[name="dispineia"]')))
 
-    context.browser.find_element_by_name(
-        "password1").send_keys("selenium-user123")
+    # wait 2
+    context.browser.implicitly_wait(3)
+    context.browser.find_element_by_css_selector(f'#risk_ranting{id_patient} input[name="dispineia"]').click()
 
-    context.browser.find_element_by_name(
-        "password2").send_keys("selenium-user123")
+    # try:
+    #     element = WebDriverWait(context.browser, 10).until(
+    #     EC.presence_of_element_located((By.CSS_SELECTOR, f'#risk_ranting{id_patient} input[name="dispineia"]'))
+    # )
+    # finally:
+    #     print("Não funfou :'( ")
+        # driver.quit()
 
-    context.browser.find_element_by_name(
-        "id_user").send_keys(id_number)
+@when(u'In modal a chose the symptons')
+def step_impl(context):
+     print(context.browser.current_url)
 
-    # dropdown state
-    Select(context.browser.find_element_by_id(
-        'dropdown-state')).select_by_value('DF')
-
-    context.browser.find_element_by_name("city").send_keys("gama")
-    context.browser.find_element_by_name("street").send_keys("alagados")
-    context.browser.find_element_by_name("neighborhood").send_keys("UnB")
-    context.browser.find_element_by_name("block").send_keys("----")
-    context.browser.find_element_by_name("number").send_keys("10")
-    context.browser.find_element_by_id('button-cadastrar').click()
-
-
-@step('I register a patient: {name_patient}, {id_number_patient} and {profile_number_patient}')
-def register_patient(context, name_patient, id_number_patient, profile_number_patient):
-    context.browser.find_element_by_id('register-patient').click()
-    Select(context.browser.find_element_by_id(
-        'id_age_range')).select_by_value('1')
-    context.browser.find_element_by_id('register-patient-button').click()
-
-
-@step('I realize a classification: {id_number_patient}')
-def classification(context, id_number_patient):
-    print (context.browser.current_url)
-    context.browser.find_element_by_id('id_modal').click()
-    context.browser.find_element_by_id('check_classication').click()
-    context.browser.find_element_by_name("dispineia").click()
-    context.browser.find_element_by_name("prostracao").click()
-
-    context.browser.find_element_by_id('modal_id').click()
-
-    context.browser.find_element_by_id('classification_save').click()
-
-
-@step('The classification must be updated')
-def verification(context):
-    title = context.browser.title
-    assert 'CRP' in title
+#
+# @then(u'Should update de classifications')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: Then Should update de classifications')
