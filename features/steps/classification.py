05aa73329d8b585be3f52a22bd3cc39b3406b1bb
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-12-12 02:36:58 -0200

    [TEST] Defining steps for patient classification

--- features/steps/classification.py
+++ features/steps/classification.py
@@ -33,7 +33,7 @@ from selenium.webdriver.common.by import By
 #     title = context.browser.title
 #     assert 'CRP' in title
 
-@when(u'I click in patient: {id_patient}')
+@step(u'I click in patient: {id_patient}')
 def acess_patient(context, id_patient):
     context.browser.find_element_by_id(f'id_modal-{id_patient}').click()
     # context.browser.find_element_by_css_selector(f'#risk_ranting{id_patient} input[name="dispineia"]').click()
@@ -42,8 +42,6 @@ def acess_patient(context, id_patient):
     # element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'#risk_ranting{id_patient} input[name="dispineia"]')))
 
     # wait 2
-    context.browser.implicitly_wait(3)
-    context.browser.find_element_by_css_selector(f'#risk_ranting{id_patient} input[name="dispineia"]').click()
 
     # try:
     #     element = WebDriverWait(context.browser, 10).until(
@@ -53,11 +51,38 @@ def acess_patient(context, id_patient):
     #     print("Não funfou :'( ")
         # driver.quit()
 
-@when(u'In modal a chose the symptons')
-def step_impl(context):
-     print(context.browser.current_url)
+@step(u'I click on Classificar')
+def classify(context):
+     # context.browser.implicitly_wait(12)
+     # element = WebDriverWait(context.browser, 10).until(
+     #     EC.presence_of_element_located((By.CSS_SELECTOR, f'#risk_ranting1 input[name="febre"]')))
 
-#
-# @then(u'Should update de classifications')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: Then Should update de classifications')
+     classify_button = context.browser.find_element_by_id('modal_id')
+     context.browser.execute_script("arguments[0].click();", classify_button)
+
+     # symptom.click()
+     # context.browser.find_element_by_link_text('Classificar').click()
+     #context.browser.find_element_by_css_selector(f'#risk_ranting{id_patient} input[name="febre"]').click()
+
+
+@step(u'I click on save')
+def click_on_save(context):
+     save_button = context.browser.find_element_by_id('modal_id')
+     context.browser.execute_script("arguments[0].click();", save_button)
+
+
+@then(u'should update de classification')
+def check_classification(context):
+    odd_rows = context.browser.find_elements_by_css_selector("tr.odd")
+    even_rows = context.browser.find_elements_by_css_selector("tr.even")
+    rows = odd_rows + even_rows
+    cells = []
+    for row in rows:
+        cells.append(row.find_elements_by_tag_name("td")[2].text)
+        # leaving this here in case you want to check all classifications
+        print (row.find_elements_by_tag_name("td")[2].text)
+
+    # The line below asserts that at least one patient is not "Não Classificado"
+    assert 'Atendimento Imediato' or \
+           'Atendimento Hospitalar' or \
+           'Atendimento Ambulatorial' in cells
